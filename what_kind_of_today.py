320fc385d17396a1bfec29af3df4b711965d75b7
Mehru Anand <Mehru@Mehrus-Air-2.fios-router.home>
2019-03-12 18:08:47 -0400

    Cleaning up commented lines

--- what_kind_of_today.py
+++ what_kind_of_today.py
@@ -12,15 +12,10 @@ repos = response.json()
 any_update = 0
 
 for repo in repos:
-    #print(repo['name'])
-    #print(repo['updated_at'])
     repo_name = repo['name']
     update_time = datetime.strptime(repo['updated_at'],"%Y-%m-%dT%H:%M:%SZ")
-    
-    #print(abs(today - update_time).days)
     if(abs(today - update_time).days < 1):
         any_update = 1
-        #print(repo_name + "was updated")
 if (any_update):
     print("You are ok - today was a push day")
 else:
