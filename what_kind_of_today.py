42310bdc83fc73315b04becaec0939583ddbb202
Mehru Anand <Mehru@Mehrus-Air-2.fios-router.home>
2019-03-15 17:03:06 -0400

    Created dict with number of commits for each date

--- what_kind_of_today.py
+++ what_kind_of_today.py
@@ -24,9 +24,20 @@ def get_commits_since(repo_name, duration):
     url_commits_since = repo['url']+'/commits?since='+ datetime.strftime(since,"%Y-%m-%dT%H:%M:%SZ")
     logger.debug("Url for commits since %s %s", duration, url_commits_since)
     commits_since = requests.get(url_commits_since).json()
+    commits_per_date = {}
+    for commit_detail in commits_since:
+        logger.info(commit_detail['commit']['committer']['date'])
+        commit_date = datetime.strptime(commit_detail['commit']['committer']['date'],"%Y-%m-%dT%H:%M:%SZ").date()
+        if commit_date in commits_per_date:
+            commits_per_date[commit_date] += 1
+        else:
+            commits_per_date[commit_date] = 1
+    for c_date, c_count in commits_per_date.items():
+        print(c_date, c_count)
     commit_count = len(commits_since) 
-    logger.debug("Number of commits since %s: %s", duration, commit_count)
+    logger.debug("Number of commits since %s day(s): %s", duration, commit_count)
     return commit_count
+
 
 
 
@@ -48,7 +59,7 @@ for repo in repos:
     update_time = datetime.strptime(repo['updated_at'],"%Y-%m-%dT%H:%M:%SZ")
     logger.info("For repo %s the last update time was %s",repo_name, update_time)
     if(abs(today - update_time).days < 1):
-        commit_count = get_commits_since(repo_name,1)
+        commit_count = get_commits_since(repo_name,3)
         any_update = 1
         if commit_count > 5:
             print("Now you are COMMITTED")
